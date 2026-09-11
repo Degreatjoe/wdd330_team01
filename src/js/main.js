00a71f32c1f472770026c62d3bb4d0914bd8c424
Great <jgreat770@gmail.com>
2026-09-11 12:57:15 +0100

    Refactor product list rendering and update HTML structure for product pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./productlist.mjs";
+import ProductList from "./ProductList.mjs"
 
-const productList = new ProductList("all", new ProductData("all"), document.getElementById("product-list"));
+const dataSource = new ProductData("tents");
+
+const element = document.querySelector(".product-list");
+
+const productList = new ProductList("Tents", dataSource, element);
+
 productList.init();
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -23,3 +23,13 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
+}
