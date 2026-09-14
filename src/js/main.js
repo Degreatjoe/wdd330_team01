20896c58b59b247267e32fd549f3e50d5f34c185
Great <jgreat770@gmail.com>
2026-09-14 13:39:48 +0100

    Fix import statement for ProductList in main.js to match file naming convention

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 import ProductData from "./ProductData.mjs";
-import ProductList from "./productList.mjs";
+import ProductList from "./productlist.mjs";
 
 const dataSource = new ProductData("tents");
 
