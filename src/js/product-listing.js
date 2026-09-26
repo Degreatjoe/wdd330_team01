1e29dad95bec35caeaf770eb1de90889cbe65b0b
braulio-v <braulio.villa.m@gmail.com>
2026-09-25 19:06:41 -0500

    Complete individual activity 03 expand inventory

---
+++ src/js/product-listing.js
@@ -0,0 +1,15 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./productlist.mjs";
+import { loadHeaderFooter, getParam } from "./utils.mjs";
+
+loadHeaderFooter();
+
+const category = getParam("category");
+
+const dataSource = new ProductData();
+
+const listElement = document.querySelector(".product-list");
+
+const myList = new ProductList(category, dataSource, listElement);
+
+myList.init();
